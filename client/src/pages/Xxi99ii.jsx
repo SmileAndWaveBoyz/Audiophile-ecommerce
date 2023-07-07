8b22ddb1770a44d275a2fa1a5833787735023bdc
SmileAndWaveBoyz <nathancaney@gmail.com>
2023-07-07 09:48:42 +0100

    Finish XX99 M II mobile page

--- client/src/pages/Xxi99ii.jsx
+++ client/src/pages/Xxi99ii.jsx
@@ -40,6 +40,42 @@ function Xx99ii() {
                         <p className='xx99miipage__list-container-item'><strong>1x</strong> 3.5mm 5m Audio Cable</p>
                         <p className='xx99miipage__list-container-item'><strong>1x</strong> Travel Bag</p>
                     </div>
+
+                    <div className="xx99miiPage-blockContainer-image-box">
+                        <img src="./assets/product-xx99-mark-two-headphones/mobile/1.png" alt="A man relaxing with headphones" />
+                        <img src="./assets/product-xx99-mark-two-headphones/mobile/2.png" alt="A table with headphones and a phone on it" />
+                        <img src="./assets/product-xx99-mark-two-headphones/mobile/3.png" alt="A close up of the XX99 Mark II Headphones" />
+                    </div>
+
+                    <div className="recomendation__block">
+                        <h3 className='recomendation__block-heading'>YOU MAY ALSO LIKE</h3>
+                        <div className="recomendation__block-container">
+                            <div className="recomendation__block-container-image-box">
+                                <img className='recomendation__block-container-image-box-image xx99mi' src="./assets/product-xx99-mark-two-headphones/mobile/mk1.png" alt="XX99 Mark I Headphones" />
+                            </div>
+
+                            <h3 className='recomendation__block-product-heading'>XX99 MARK I</h3>
+                            <Link to={"/headphones/xx99mi"} className="seeProductButton">SEE PRODUCT</Link>
+                        </div>
+
+                        <div className="recomendation__block-container">
+                            <div className="recomendation__block-container-image-box">
+                                <img className='recomendation__block-container-image-box-image xx59' src="./assets/product-xx99-mark-two-headphones/mobile/xx59.png" alt="XX99 Mark I Headphones" />                                
+                            </div>
+
+                            <h3 className='recomendation__block-product-heading'>XX59</h3>
+                            <Link to={"/headphones/xx59"} className="seeProductButton">SEE PRODUCT</Link>
+                        </div>
+
+                        <div className="recomendation__block-container">
+                                <div className="recomendation__block-container-image-box">
+                                    <img className='recomendation__block-container-image-box-image zx9' src="./assets/product-zx9-speaker/mobile/zx9.png" alt="XX99 Mark I Headphones" />                                
+                                </div>
+                            <h3 className='recomendation__block-product-heading'>ZX9 SPEAKER</h3>
+                            <Link to={"/speakers/zx9"} className="seeProductButton">SEE PRODUCT</Link>
+                        </div>
+                    </div>
+                    
                 </div>   
 
                 <Links/>
