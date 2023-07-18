66765353ca089c3492002115179e4a31c55ec587
SmileAndWaveBoyz <nathancaney@gmail.com>
2023-07-18 17:42:50 +0100

    Trying redux

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,17 @@ import ScrollToTop from './ScrollToTop';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ScrollToTop />
-      <App />
+      <ScrollToTop />
+        <Provider store={store}>
+          <App />
+        </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
