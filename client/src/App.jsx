c274708c69edadf1d9c4b9e4e06bf589b204ebb8
Nathan Caney <nathancaney@gmail.com>
2023-07-25 18:27:11 +0100

    Better console logging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -87,7 +87,7 @@ function App() {
   const yx1Quantity = useSelector(selectYx1Quantity)
 
 
-  function updateBackEnd() {
+  function updateBackEnd() { // This will update the back
     let itemNameToUpdate = ["XX99 MARK II Headphones", "XX99 MARK I Headphones", "XX59 Headphones", "ZX9 Speaker", "ZX7 Speaker", "YX1 Wireless Earphones"];
     let itemQuantityArray = [xx99mk2Quantity, xx99mk1Quantity, xx59Quantity, zx9Quantity, zx7Quantity, yx1Quantity]
 
